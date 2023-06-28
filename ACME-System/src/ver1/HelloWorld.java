70a311115bb931469aed803fed155d26af71ec54
K3ny3tta <aaliyahliyah.aw@gmail.com>
2023-06-28 16:29:52 -0400

    whatFor method Added

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -1,17 +1,27 @@
 package ver1;
-
+import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
 
 		System.out.println("Gibby is working locally on his branch");
-
+		
 
 	}
 	public static void heyNow() {
 		String Msg = "Hey Now!";
 		System.out.println(Msg);
+	}
+	
+	public static void whatFor() {
+		Scanner scn = new Scanner (System.in);
+		System.out.println("Type message.");
+		
+		String message = scn.nextLine();
+		System.out.println (message +", what for?");
+			
+	
 	}
 
 
