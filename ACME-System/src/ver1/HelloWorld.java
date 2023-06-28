8c47c7aa3a7a804300296bc2672e07ff8b828c78
K3ny3tta <aaliyahliyah.aw@gmail.com>
2023-06-28 14:06:20 -0400

    Added new line

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
 
+		System.out.println("Gibby is working locally on his branch");
+
 
 	}
 
